cbcf1f2d44e3e71d016c02cc0af9a352e862a820
rafael cocone <rafaelcocone@gmail.com>
2021-07-20 12:16:29 -0500

    ejemplo de vue

---
+++ ejemplo-vue/src/components/nav.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="nav">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'nav',
+  data () {
+    return {
+      msg: 'Hola mundoww'
+    }
+  }
+}
+</script>
+
+
